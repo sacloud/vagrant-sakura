1bab5bffc86af6136a3f5b18eb443ade14df81b1
Tomoyuki Sahara <sahara@surt.net>
2014-04-19 20:43:35 +0900

    add synopsis of sakura-list-id command.

--- lib/vagrant-sakura/command.rb
+++ lib/vagrant-sakura/command.rb
@@ -1,6 +1,10 @@
 module VagrantPlugins
   module Sakura
     class Command < Vagrant.plugin(2, :command)
+      def self.synopsis
+        "query Sakura for available archives and server plans"
+      end
+
       def execute
         with_target_vms(nil, { :provider => "sakura" }) do |vm|
           vm.action(:list_id)
